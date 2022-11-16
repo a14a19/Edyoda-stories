c8bc6d79935bd0446681e5c75c9f2bd38bd063ad
a14a19 <anas31197@gmail.com>
2022-11-17 00:29:55 +0530

    Initial Commit

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,36 @@
-import './card.css';
+import classes from './card.module.scss';
 import Data from './card.json';
 
 console.log(Data);
 
+const cardDetail = Data.map((items, i) => {
+    const { image, date, name, title, text } = items
+    // console.log(image, date, name, title, text);
+    return(
+        <div className={classes.card} key={i}>
+            <img src={image} alt='...'/>
+            <div className={classes.title}>
+                {title}
+            </div>
+            <div className={classes.nameDate}>
+                <span>
+                    {name}
+                </span>
+                <span>
+                    {date}
+                </span>
+            </div>
+            <div className={classes.text}>
+                {text}
+            </div>
+        </div>
+    )
+})
+
 function Card() {
     return(
-        <div className='m-margin'>
+        <div className={classes.cardContainer}>
+            {cardDetail}
         </div>
     )
 }
